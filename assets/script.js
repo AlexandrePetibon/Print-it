3bafdc2de6da812dcdaa813f4e684ea81330e0ca
Alexandre Petibon <alexandre.petibon@outlook.fr>
2023-03-31 15:30:19 +0200

    avancement du carrousel, reste à débloquer dot_selected

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,58 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"/assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"/assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"/assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"/assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
 
-const leftArrow = document.getElementById('left_arrow');
-const rightArrow = document.getElementById('right_arrow');
+const leftArrow = document.querySelector('.arrow_left')
+const rightArrow = document.querySelector('.arrow_right')
+const dots = document.querySelector('.dots')
+const img = document.querySelector('.banner-img')
+let positionSlide = 0
+console.log(img)
+const txt = document.querySelector('.banner-txt')
 
+leftArrow.addEventListener('click', function() {
+	positionSlide--
+	if ( positionSlide ===-1) {
+		positionSlide=slides.length -1
 
-leftArrow.addEventListener('click', () => {
-	console.log('On clique sur la flèche de gauche');
-});
+	}
+	img.src= slides[positionSlide].image
+	txt.innerHTML = slides[positionSlide].tagLine
+	console.log('On clique sur la flèche de gauche')
+})
+rightArrow.addEventListener('click', function() {
+	positionSlide++
+	if ( positionSlide === slides.length) {
+		positionSlide=0
+	}
+	img.src= slides[positionSlide].image 
+	txt.innerHTML = slides[positionSlide].tagLine
+	console.log('On clique sur la flèche de droite')
+})
 
-rightArrow.addEventListener('click', () => {
-	console.log('On clique sur la flèche de droite');
-});
+for (let i = 0 ; i < slides.length ; i++) {
+	const newDot = document.createElement('div')
+	newDot.classList='dot'
+	dots.appendChild(newDot)
+	console.log('.dot')
+}
+	
+let dotVide = document.querySelector('.dot')
+
+  dotVide.classList.add('dot_selected')
+
